c67e0348816c7f40bbb4320087921b8c334bfaea
tymosh <77159957+tymoshh@users.noreply.github.com>
2024-08-10 22:05:48 +0100

    Update win-install.bat

--- win-install.bat
+++ win-install.bat
@@ -42,7 +42,7 @@ mkdir bins
 
 curl -L -o SDL2.tar.gz https://github.com/libsdl-org/SDL/releases/download/release-2.30.5/SDL2-devel-2.30.5-mingw.tar.gz
 tar -xvzf SDL2.tar.gz
-for /d %%i in (SDL2-*) do (
+for /d %%i in (SDL2*) do (
   set "folderName=%%~nxi"
 )
 xcopy "!folderName!\x86_64-w64-mingw32\bin\SDL2.dll" "%cd%\bins\" /e
@@ -51,7 +51,7 @@ del SDL2.tar.gz
 
 curl -L -o SDL2_image.tar.gz https://github.com/libsdl-org/SDL_image/releases/download/release-2.8.2/SDL2_image-devel-2.8.2-mingw.tar.gz
 tar -xvzf SDL2_image.tar.gz
-for /d %%i in (SDL2-*) do (
+for /d %%i in (SDL2*) do (
   set "folderName=%%~nxi"
 )
 xcopy "!folderName!\x86_64-w64-mingw32\bin\SDL2_image.dll" "%cd%\bins\" /e
@@ -60,7 +60,7 @@ del SDL2_image.tar.gz
 
 curl -L -o SDL2_mixer.tar.gz https://github.com/libsdl-org/SDL_mixer/releases/download/release-2.8.0/SDL2_mixer-devel-2.8.0-mingw.tar.gz
 tar -xvzf SDL2_mixer.tar.gz
-for /d %%i in (SDL2-*) do (
+for /d %%i in (SDL2*) do (
   set "folderName=%%~nxi"
 )
 xcopy "!folderName!\x86_64-w64-mingw32\bin\SDL2_mixer.dll" "%cd%\bins\" /e
@@ -69,7 +69,7 @@ del SDL2_mixer.tar.gz
 
 curl -L -o SDL2_ttf.tar.gz https://github.com/libsdl-org/SDL_ttf/releases/download/release-2.22.0/SDL2_ttf-devel-2.22.0-mingw.tar.gz
 tar -xvzf SDL2_ttf.tar.gz
-for /d %%i in (SDL2-*) do (
+for /d %%i in (SDL2*) do (
   set "folderName=%%~nxi"
 )
 xcopy "!folderName!\x86_64-w64-mingw32\bin\SDL2_ttf.dll" "%cd%\bins\" /e
